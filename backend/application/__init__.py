add34771bb52afde1c5dfd1926f8dca1b2d82e68
theophilus <theophilus.ogbolu@gmail.com>
2023-05-01 23:20:52 +0100

    webpack

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_webpack import Webpack
 
 from .api import bp as api
 from .api.chat import bp as chat
@@ -11,6 +12,7 @@ def create_app(config_file="config.py"):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
     CORS(app)
+    Webpack(app)
 
     app.register_blueprint(api)
     app.register_blueprint(chat)
